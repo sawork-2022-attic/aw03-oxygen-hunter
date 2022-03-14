7594d4e95243072997a39964c69ec498171fc821
m1rr0r <xiao.li@smail.nju.edu.cn>
2022-03-15 00:14:12 +0800

    adding subProduct

--- src/main/java/com/example/webpos/web/PosController.java
+++ src/main/java/com/example/webpos/web/PosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PosController {
@@ -23,5 +24,11 @@ public class PosController {
         model.addAttribute("products", posService.products());
         model.addAttribute("cart", posService.getCart());
         return "index";
+    }
+
+    @GetMapping("/sub")
+    public String subProdcut(@RequestParam("itemIndex") int itemIndex) {
+
+        return "redirect:/";
     }
 }
